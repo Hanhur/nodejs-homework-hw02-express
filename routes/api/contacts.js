dd3a1855dc724f03c2b66f8c6963fbeb4fc33917
Vladimir Hanhur <aqgz@email.cz>
2023-10-05 10:58:12 +0200

    update

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -5,9 +5,13 @@ import { validateData } from '../../helpers/validateData.js';
 const router = express.Router();
 
 router.get('/', listContacts);
+
 router.get('/:contactId', getContactById);
+
 router.post('/', validateData, addContact);
+
 router.delete('/:contactId', removeContact);
+
 router.put('/:contactId', validateData, updateContactById);
 
 export default router;
