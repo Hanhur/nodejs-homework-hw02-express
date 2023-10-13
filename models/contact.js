981a33a74ed571af39fd723623a5aedcf9250b30
Vladimir Hanhur <aqgz@email.cz>
2023-10-13 22:42:04 +0200

    update

---
+++ models/contact.js
@@ -0,0 +1,36 @@
+const { Schema, model } = require("mongoose");
+const handleMongooseError = require("../helpers/handleMongooseError");
+const {
+    nameRegExp,
+    phoneRegExp,
+} = require("../utils/validation/contactValidation");
+
+const contactSchema = new Schema(
+    {
+        name: {
+            type: String,
+            required: [true, "Set name for contact"],
+            match: RegExp(nameRegExp),
+        },
+        email: {
+            type: String,
+            required: true,
+        },
+        phone: {
+            type: String,
+            required: true,
+            match: RegExp(phoneRegExp),
+        },
+        favorite: {
+            type: Boolean,
+            default: false,
+        },
+    },
+    { versionKey: false, timestamps: true }
+);
+
+contactSchema.post("save", handleMongooseError);
+
+const Contact = model("contact", contactSchema);
+
+module.exports = { Contact };
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,13 +1,24 @@
 const express = require("express");
 const router = express.Router();
 const ctrl = require("../../controllers/contacts");
-const { validateBody } = require("../../middlewares/validateBody");
-const schemas = require("../../utils/validation/contactValidation");
+const { validateBody, isValidId } = require("../../middlewares");
+const { schemas } = require("../../utils/validation/contactValidation");
 
 router.get("/", ctrl.getAll);
-router.get("/:id", validateBody(schemas.addSchema), ctrl.getContactById);
+
+router.get("/:id", isValidId, ctrl.getContactById);
+
 router.post("/", validateBody(schemas.addSchema), ctrl.postContact);
-router.delete("/:id", ctrl.deleteContact);
-router.put("/:id", ctrl.putContact);
+
+router.delete("/:id", isValidId, ctrl.deleteContact);
+
+router.put("/:id", isValidId, validateBody(schemas.addSchema), ctrl.putContact);
+
+router.patch(
+    "/:id/favorite",
+    isValidId,
+    validateBody(schemas.updateFavoriteSchema),
+    ctrl.updateFavorite,
+);
 
 module.exports = router;
